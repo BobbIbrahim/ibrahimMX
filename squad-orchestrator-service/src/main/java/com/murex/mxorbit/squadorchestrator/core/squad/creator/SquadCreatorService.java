c90d21ef3d12cb4817b1cc7d9afd0370e29139a0
IBRAHIM Ibrahim <iibrahim@murex.com>
2026-07-17 18:16:36 +0300

    Add Validation tests

--- squad-orchestrator-service/src/main/java/com/murex/mxorbit/squadorchestrator/core/squad/creator/SquadCreatorService.java
+++ squad-orchestrator-service/src/main/java/com/murex/mxorbit/squadorchestrator/core/squad/creator/SquadCreatorService.java
@@ -4,6 +4,7 @@ import com.murex.mxorbit.squadorchestrator.core.squad.creator.mapper.SquadCreato
 import com.murex.mxorbit.squadorchestrator.core.squad.model.Squad;
 import com.murex.mxorbit.squadorchestrator.core.squad.creator.request.CreateSquadRequest;
 import com.murex.mxorbit.squadorchestrator.core.squad.store.SquadStore;
+import com.murex.mxorbit.squadorchestrator.core.squad.validation.SquadInputRefValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,10 +16,12 @@ public class SquadCreatorService implements SquadCreator {
 
 	private final SquadStore squadStore;
 	private final SquadCreatorMapper squadCreatorMapper;
+	private final SquadInputRefValidator squadInputRefValidator;
 
 	@Override
 	public Squad createSquad(CreateSquadRequest request) {
 		log.debug("Creating squad with request: {}", request);
+		squadInputRefValidator.validate(request);
 		Squad squad = squadStore.save(squadCreatorMapper.toCreateSquadStoreRequest(request));
 		log.info("Squad created: {}", squad);
 		return squad;
